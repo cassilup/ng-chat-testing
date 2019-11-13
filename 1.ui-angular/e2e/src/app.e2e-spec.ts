d478fb0f9a7a248c18d2f57f03981fe2c97ec93b
Cassian Lup <cassi.lup@gmail.com>
2019-11-13 23:14:13 +0100

    some more e2e tests as basis for exercises

--- 1.ui-angular/e2e/src/app.e2e-spec.ts
+++ 1.ui-angular/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,10 +10,38 @@ describe('workspace-project App', () => {
 
   it('should start at the login page', async () => {
     page.navigateTo();
-    browser.waitForAngular();
+    // browser.sleep(100000);
     const currentUrl = await browser.getCurrentUrl()
     expect(currentUrl).toContain("/login");
   });
+
+  it('should login and see the messages page', async () => {
+    page.navigateTo('/login');
+
+    // type in login data
+    element(by.css('.login-form #username')).sendKeys('admin')
+    element(by.css('.login-form #password')).sendKeys('pass')
+    browser.waitForAngularEnabled(false);
+    element(by.css('.login-form form')).submit();
+    // browser.waitForAngularEnabled(true);
+
+    const currentUrl = await browser.getCurrentUrl();
+    expect(currentUrl).toContain("/home");
+  });
+
+  it('should be able to send a message and check that it appears in the list', async () => {
+  });
+
+  it('should refresh page and check that the same number of messages appear (persistance)', async () => {
+  });
+
+  it('should check that appropriate classes are present in the header', () => {
+  });
+
+  it('should check that home is only accessible to logged in users', () => {
+    // https://blog.cloudboost.io/building-your-first-tests-for-angular5-with-protractor-a48dfc225a75
+  });
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
